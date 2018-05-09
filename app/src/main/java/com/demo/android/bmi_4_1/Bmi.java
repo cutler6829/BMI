e0bbb256e6c14c940adc974db41263c133d0f6ec
cutler6829 <asplion6829@gmail.com>
2018-05-09 14:35:24 +0800

    toggle_state_list_v1.0

--- app/src/main/java/com/demo/android/bmi_4_1/Bmi.java
+++ app/src/main/java/com/demo/android/bmi_4_1/Bmi.java
@@ -4,37 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.text.DecimalFormat;
 
-public class Bmi extends AppCompatActivity implements View.OnClickListener {
+public class Bmi extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-    EditText field_height;
-    EditText field_weight;
-    TextView result;
-    TextView suggest;
-    Button submit;
+    //EditText field_height;
+    //EditText field_weight;
+    //TextView result;
+    //TextView suggest;
+    //Button submit;
+    ToggleButton tbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Button submit = findViewById(R.id.submit);
-        setContentView(R.layout.activity_bmi);
-        findViews();
-        submit.setOnClickListener(this);
-    }
-    private void findViews(){ //導向物件欄位位置，注意名稱一定要相同
-        submit =findViewById(R.id.submit);
-        field_height= findViewById(R.id.field_height);
-        field_weight= findViewById(R.id.field_weight);
-        result=findViewById(R.id.result);
-        suggest=findViewById(R.id.suggest);
+        setContentView(R.layout.my_button_state);
+        //findViews();
+        //submit.setOnClickListener(this);
+        tbtn = findViewById(R.id.toggleButton);
+        tbtn.setOnCheckedChangeListener(this);
     }
 
-
-    @Override
+    //private void findViews() { //導向物件欄位位置，注意名稱一定要相同
+        //submit = findViewById(R.id.submit);
+        //field_height = findViewById(R.id.field_height);
+        //field_weight = findViewById(R.id.field_weight);
+        //result = findViewById(R.id.result);
+        //suggest = findViewById(R.id.suggest);
+    //}
+    /*@Override
     public void onClick(View v) {
 
         String height=field_height.getText().toString();
@@ -54,6 +59,14 @@ public class Bmi extends AppCompatActivity implements View.OnClickListener {
         }else
             suggest.setText(R.string.advice_average);
 
-    }
+    }*/
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked)
+            Toast.makeText(Bmi.this,"now is on",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(Bmi.this,"now is off",Toast.LENGTH_SHORT).show();
+
+    }
 }
